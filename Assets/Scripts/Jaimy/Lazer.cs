376308665d0712d135e3853c7a678ed1a5702c7b
Spraxs <jaimy.putter@outlook.com>
2018-12-14 10:26:09 +0100

    Created shooting system

---
+++ Assets/Scripts/Jaimy/Lazer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lazer : MonoBehaviour {
+
+    public void Shoot(GameObject projectile, GameObject ship) {
+
+        RaycastHit hit;
+
+        Vector3 position = gameObject.transform.position + ship.transform.forward * 5;
+
+
+        if (!Physics.Raycast(position, ship.transform.forward, out hit)) return;
+        
+        GameObject proj = Instantiate(projectile, gameObject.transform.position, ship.transform.rotation);
+
+        Projectile projectileScript = proj.GetComponent<Projectile>();
+
+        projectileScript.ship = ship;
+
+        projectileScript.destination = hit.point;
+    }
+}
