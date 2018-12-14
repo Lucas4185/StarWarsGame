0336a6ff0b90be5caa972f3100eda0a5c173e1fc
AlexJanson <alex@hoofddorp.nl>
2018-12-14 16:25:35 +0100

    player angles on circle

--- Assets/Scripts/Alex/SpaceFlight.cs
+++ Assets/Scripts/Alex/SpaceFlight.cs
@@ -7,6 +7,7 @@ public class SpaceFlight : MonoBehaviour {
     Rigidbody pilot;
     ConstantForce force;
     Vector3 pointPosition;
+    GameObject point;
     
     bool invertPitch = true;
     bool invertYaw = false;
@@ -19,21 +20,38 @@ public class SpaceFlight : MonoBehaviour {
     void Start () { 
         pilot = GetComponent<Rigidbody>();
         force = GetComponent<ConstantForce>();
+
+         point = new GameObject("Hover Point");
+        //  point.AddComponent<Transform>();
     }
 
     void FixedUpdate() {
         //transfrom.rotation = Quaternion.LookAt(transfrom.position - (Camera.main.transfrom.position + offset));
         pointPosition = Camera.main.transform.position + offset + Camera.main.transform.forward * distance;
+        point.transform.position = pointPosition;
 
         transform.rotation = Quaternion.Lerp(transform.rotation, Camera.main.transform.rotation, Time.deltaTime * rotationTime);
 
         Vector3 pointToMove = Vector3.Lerp(transform.position, pointPosition, Time.deltaTime * smooth);
 
-        Vector3 heading = transform.position - pointPosition;
-        float dist = heading.magnitude;
-        Vector3 direction = heading / dist;
+        // Vector3 a = (Camera.main.transform.position - transform.position);
+        // Vector3 b = (Camera.main.transform.position - pointPosition);
+
+        // Debug.DrawLine(transform.position, a, Color.blue);
+        // Debug.DrawLine(transform.position, b, Color.blue);
+
+        // float inproduct = a.x * b.x + a.y * b.y;
+
+        // float theta = Mathf.Acos(inproduct);
+        // SetRoll(theta);
+
+        #region distance
+        // Vector3 heading = transform.position - pointPosition;
+        // float dist = heading.magnitude;
+        // Vector3 direction = heading / dist;
 
         //SetRoll((dist * direction.z) * 10);
+        #endregion
 
         pilot.MovePosition(pointToMove);
     }
---
+++ Assets/Testing.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour {
+
+	public Transform ship;
+	Transform point;
+
+	Vector2 a,b;
+
+	void Update() {
+		if(point == null) {
+			point = GameObject.Find("Hover Point").transform;
+		}
+		a = new Vector2(transform.position.x - point.position.x, transform.position.z - point.position.z);
+		b = new Vector2(transform.position.x - ship.position.x, transform.position.z - ship.position.z);
+
+		print(a.x + " - " + b.x);
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.DrawSphere(new Vector3(a.x, 0f, a.y), 0.5f);
+		Gizmos.DrawSphere(new Vector3(b.x, 0f, b.y), 0.5f);
+	}
+
+}
