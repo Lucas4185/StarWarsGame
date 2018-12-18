a32b7b6f258b8fbc9bf732a6b42d48197eddaa76
Spraxs <jaimy.putter@outlook.com>
2018-12-18 11:44:27 +0100

    Made some changes to player shooting

--- Assets/Scripts/Jaimy/Lazer.cs
+++ Assets/Scripts/Jaimy/Lazer.cs
@@ -10,7 +10,7 @@ public class Lazer : MonoBehaviour {
 
         Vector3 position = gameObject.transform.position + ship.transform.forward * 5;
 
-        if (!Physics.Raycast(position, ship.transform.forward, out hit)) return;
+        Physics.Raycast(position, ship.transform.forward, out hit, 500f);
         
         GameObject proj = Instantiate(projectile, gameObject.transform.position, ship.transform.rotation);
 
